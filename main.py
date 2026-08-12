2e9e6249a5853a2772084dc142245805e6f2f80d
Shuchi <sawhney.shuchi@gmail.com>
2026-08-12 11:38:30 +0530

    Intro to search agent

--- main.py
+++ main.py
@@ -1,32 +1,23 @@
 from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
 from langchain_groq import ChatGroq
+from langchain_tavily import TavilySearch
 
 load_dotenv()
 
+llm = ChatGroq(model="llama-3.1-8b-instant",temperature=0)
+
+tools = [TavilySearch(max_results=3, search_type="web", search_mode="realtime")]
+#agent = create_agent(model="llama-3.1-8b-instant",tools=tools,system_prompt="You are a helpful assistant. When the user asks for current or real-time information, always use the search tool.")
+agent = create_agent(model=llm,tools=tools,system_prompt="You are a helpful assistant. When the user asks for current or real-time information, always use the search tool.")
 
 def main():
     print("Hello From Langchain!")
-    information="""
-    Hritik Rakesh Nagrath[1] (born 10 January 1974), known professionally as Hrithik Roshan (Hindi: [ˈɾɪt̪ɪk ɾoːʃən][2]), is an Indian actor and producer who works in Hindi cinema. Referred to as the millennial superstar,[3] he has portrayed a variety of characters and is known for his dancing skills. One of the highest-paid actors in India, he has won many awards, including six Filmfare Awards, of which four were for Best Actor. Starting from 2012, he has appeared in Forbes India's Celebrity 100 several times based on his income and popularity.
-
-    Roshan has frequently collaborated with his father, Rakesh Roshan. He made brief appearances as a child actor in several films in the 1980s and later worked as an assistant director on four of his father's films. His first leading role was in the box-office success Kaho Naa... Pyaar Hai (2000), for which he received several awards. Performances in the 2000 terrorism drama Fiza and the 2001 ensemble family drama Kabhi Khushi Kabhie Gham consolidated his reputation but were followed by several poorly received films.
-    """
-
-    summary_template = """
-    You are a summarization engine. Your task is to summarize the following information in 2 sentences.
-    Information: {information}
-    """
-
-    summary_prompt = PromptTemplate.from_template(summary_template)
-
-    llm = ChatGroq(model_name="llama-3.1-8b-instant", temperature=0.7)
-
-    chain = summary_prompt | llm
-
-    response = chain.invoke({"information": information})
-
-    print(response.content)
+    result = agent.invoke({"messages": [HumanMessage(content="Search the job openings for EA Assistant in Delhi/NCR location")]})
+    print(result["messages"][-1].content)
 
 if __name__ == "__main__":
+
     main()
